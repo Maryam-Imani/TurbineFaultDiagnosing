8048324133d5ab62ba250c7887a3223df5bcc5c8
Maryam-Imani <imani.ma@gmail.com>
2017-03-27 15:34:31 -0500

    Add files via upload

---
+++ src/producer.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 23 15:06:02 2016
+
+
+python producer.py /home/maryam/Desktop/Kafka/SparkDBFE.txt 10.176.148.123 9092
+
+@author: mary
+"""
+
+#simple_producer.py
+from kafka import KafkaProducer
+from time import sleep
+import sys, time
+
+if __name__ == "__main__":
+    
+    filename= sys.argv[1]
+    ip = sys.argv[2]
+    port = sys.argv[3]
+    producer = KafkaProducer(bootstrap_servers= ip+':'+port)
+    
+    with open(filename,'r') as f:
+        lines= f.readlines()
+    
+    producer.send('test', 'start')
+    
+#    while True:
+    for i in range(0, 1):
+        startTime = time.time()
+        for line in lines: 
+            producer.send('test', line)
+            sleep(0.001)
+    
+    print (time.time()- startTime)
+    producer.send('test', 'end')
